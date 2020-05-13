79498a8339a3b4027001b8b1ea6095c9c1439f30
Jerry Yang <jerry871002@gmail.com>
2020-05-13 14:43:39 +0800

    Update play_with_model.py

--- play_with_model.py
+++ play_with_model.py
@@ -14,10 +14,9 @@ if len(sys.argv) != 4:
     print('Usage: python play_with_model.py <model-path> <episode> <deterministic>')
     sys.exit()
 else:
-    moving_avg_file = sys.argv[1]
-    model_path = sys.argv[2]
-    episode = sys.argv[3]
-    isDeterministic = bool(int(sys.argv[4]))
+    model_path = sys.argv[1]
+    episode = sys.argv[2]
+    isDeterministic = bool(int(sys.argv[3]))
 
 # set environment
 env = SoccerEnv(width=5, height=5, goal_size=3)
@@ -30,11 +29,10 @@ critic = f'agentL_{agent_type}_{episode}_LR_1e-05_critic.h5'
 agentL.load_weights(path_actor=model_path+actor, path_critic=model_path+critic)
 
 # parameters
-EPISODES = 10
+EPISODES = 3
 
 for i in range(EPISODES):
     state = env.reset()
-    stat.set_initial_ball(state[4])
 
     stateL, stateR = state_each(normalize(env, state)) # for MADDPG agent
 
@@ -68,8 +66,6 @@ for i in range(EPISODES):
 
         # perform actions on the environment
         done, reward_l, reward_r, state_, actions = env.step(actionL, actionR)
-
-        agentR.adjust(done, reward_r, i)
 
         state = state_
         stateL, stateR = state_each(normalize(env, state))
